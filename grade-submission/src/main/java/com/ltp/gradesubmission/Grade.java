ba2e417ca8a32f80514bb026e3805f6458fc6abd
Tim Klimpel <tim.klimpel@gmail.com>
2023-01-08 12:17:17 -0500

    Complete Section 8 (grade-submission project complete)

--- grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,18 +1,22 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grade {
     private String name;
     private String subject;
     private String score;
+    private String id;
 
 
-    public Grade(String name, String subject, String score) {
-        this.name = name;
-        this.subject = subject;
-        this.score = score;
-    }
+    // public Grade(String name, String subject, String score) {
+    //     this.name = name;
+    //     this.subject = subject;
+    //     this.score = score;
+    // }
 
     public Grade() {
+        this.id = UUID.randomUUID().toString();
     }
 
 
@@ -38,6 +42,14 @@ public class Grade {
 
     public void setScore(String score) {
         this.score = score;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
 
--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,19 +7,51 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"),
-        new Grade("Hermoine", "Arithmancy", "A+"),
-        new Grade("Neville", "Charms", "A-")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
+
+    @GetMapping("/")
+    public String gradeForm(Model model, @RequestParam(required = false) String id){
+        Grade grade;
+        int index = getGradeIndex(id);
+        if(index == Constants.NOT_FOUND){
+            grade = new Grade();
+        } else {
+            grade = studentGrades.get(index);
+        }
+        model.addAttribute("grade", grade);
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitGrade(Grade grade){
+        int index = getGradeIndex(grade.getId());
+        if(index == Constants.NOT_FOUND){
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
+        return "redirect:/grades";
+    }
     
     @GetMapping("/grades")
     public String getGrades(Model model) {
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    public Integer getGradeIndex(String id){
+        for(int i = 0; i < studentGrades.size(); i++){
+            if(studentGrades.get(i).getId().equals(id)){
+                return i;
+            }
+        }
+
+        return Constants.NOT_FOUND;
     }
 }
